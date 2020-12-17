8fe22f1581ea2421982e73f09a4b14f1c0fd1ea8
Marzuki <marzukiberg@gmail.com>
2020-12-17 10:03:41 +0700

    added comments section

--- js/script.js
+++ js/script.js
@@ -1,21 +1,92 @@
+// init
 const $ = (el) => document.querySelector(el);
-const likeButton = $("#like-button"),
+const reactCounter = $("#react-counter"),
+  likeButton = $("#like-button"),
+  commentsButton = $("#comments-button"),
+  commentsSection = $(".comments-section"),
+  btnCloseComments = $("#btn-close-comments"),
   footerAction = $(".footer-action"),
-  moreReactButtons = $(".more-react-buttons");
+  moreReactButtons = $(".more-react-buttons"),
+  like = $("#like"),
+  love = $("#love");
+let state = {
+  likes: 1322,
+  react: null,
+};
 
-likeButton.addEventListener("click", (e) => {
-  alert("hello");
-});
-likeButton.addEventListener("long-press", (e) => {
-  showMoreReacts();
-});
-window.addEventListener("mouseup", () => {
-  if (footerAction.classList.contains("show")) {
-    showMoreReacts();
+// function
+function updateState() {
+  switch (state.react) {
+    case null:
+      likeButton.innerHTML = `<i class="fa fa-thumbs-o-up"></i>
+      <span>Suka</span>`;
+      break;
+    case "like":
+      likeButton.innerHTML = `<img src="images/like.png" width="16" height="16">
+      <span class="blue">Suka</span>`;
+      toggleMoreReacts();
+      break;
+    case "love":
+      likeButton.innerHTML = `<img src="images/love.png" width="16" height="16">
+      <span class="pink">Super</span>`;
+      toggleMoreReacts();
+      break;
+    case "laugh":
+      likeButton.innerHTML = `<img src="images/laugh.png" width="16" height="16">
+      <span class="yellow">Haha</span>`;
+      toggleMoreReacts();
+      break;
+    case "sad":
+      likeButton.innerHTML = `<img src="images/sad.png" width="16" height="16">
+        <span class="yellow">Sedih</span>`;
+      toggleMoreReacts();
+      break;
+    case "angry":
+      likeButton.innerHTML = `<img src="images/angry.png" width="16" height="16">
+          <span class="yellow">Marah</span>`;
+      toggleMoreReacts();
+      break;
+    default:
+      break;
   }
-});
-moreReactButtons.addEventListener("mouseup", (e) => e.stopPropagation());
-
-function showMoreReacts() {
-  footerAction.classList.toggle("show");
 }
+
+function toggleMoreReacts() {
+  footerAction.classList.toggle("show-react");
+}
+function toggleLike() {
+  likeButton.innerHTML = `<img src="images/like.png" width="16" height="16">
+  <span class="blue">Suka</span>`;
+  toggleMoreReacts();
+}
+function toggleLove() {
+  likeButton.innerHTML = `<img src="images/love.png" width="16" height="16">
+  <span class="pink">Super</span>`;
+  toggleMoreReacts();
+}
+function toggleComments() {
+  commentsSection.classList.toggle("show");
+}
+
+// trigger
+likeButton.addEventListener("long-press", toggleMoreReacts);
+likeButton.addEventListener("click", () => {
+  state.react = state.react == null ? "like" : null;
+  updateState();
+});
+window.addEventListener(
+  "mouseup",
+  () => footerAction.classList.contains("show-react") && toggleMoreReacts()
+);
+moreReactButtons.addEventListener("mouseup", (e) => e.stopPropagation());
+commentsButton.addEventListener("click", toggleComments);
+btnCloseComments.addEventListener("click", toggleComments);
+
+like.addEventListener("click", () => ((state.react = "like"), updateState()));
+love.addEventListener("click", () => ((state.react = "love"), updateState()));
+laugh.addEventListener("click", () => ((state.react = "laugh"), updateState()));
+wow.addEventListener("click", () => ((state.react = "wow"), updateState()));
+sad.addEventListener("click", () => ((state.react = "sad"), updateState()));
+angry.addEventListener("click", () => ((state.react = "angry"), updateState()));
+
+updateState();
